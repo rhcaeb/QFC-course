17fce29df81fc751701ba22bab2c2335b62472ef
Ryan Beach <beachr90@gmail.com>
2019-10-27 12:20:32 -0400

    Start Lesson 1-2 variables; TO BE COMPLETED

---
+++ _scripts/lesson1-2.R
@@ -0,0 +1,38 @@
+## ---------------------------
+##
+## Lesson: 
+##
+## 1-2: Variables
+##
+## Script:
+##
+## lesson1-2.R
+##
+## Author: Ryan Beach
+##
+## Date Created: 2019-10-27
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------  
+
+{
+  # the next two lines should be at the top of all your scripts
+  
+  rm(list=ls());
+  options(show.error.messages = TRUE);
+  
+  # create three varibles: d, t, and v
+  
+  # give d and t values and use them to calculate v
+  
+  d <- 100; # distance
+  t <- 20; # time
+  v <- d/t; # solve for velocity
+  
+}
+
+
