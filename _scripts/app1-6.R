26b205c95a36546185c5a47fcc38b7aa6b92f975
Ryan Beach <ryan.beach@ontario.ca>
2019-11-29 15:47:50 -0500

    complete lesson 1-6 application

---
+++ _scripts/app1-6.R
@@ -0,0 +1,28 @@
+{
+  
+  rm(list=ls());
+  options(show.error.locations = TRUE);
+  
+ # Prompt user to input location 
+  
+  tempLoc <- readline("Where were the measurements recorded?: ");
+  
+ # Prompt user to input recorded measurements
+  
+  temp1 <- as.numeric(readline("Enter the first measurement: "));
+  temp2 <- as.numeric(readline("Enter the second measurement: "));
+  temp3 <- as.numeric(readline("Enter the third measurement: "));
+  temp4 <- as.numeric(readline("Enter the fourth measurement: "));
+  temp5 <- as.numeric(readline("Enter the fifth measurement: "));
+  
+  # Calculate mean temperature
+  
+  mnTemp <- sum(temp1, temp2, temp3, temp4, temp5)/5; # n = 5
+  
+  # Summarize in console
+  
+  cat("The temperature measurements are from: ", tempLoc,
+      "\nThe recorded temperatures were: ", temp1, temp2, temp3, temp4, temp5,
+      "\nThe average temperature was: ", mnTemp)
+
+}
