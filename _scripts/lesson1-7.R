7f49b813363ae306585d99e9b7f3f1270b5baabc
Ryan Beach <ryan.beach@ontario.ca>
2019-12-13 16:03:45 -0500

    complete lesson for 1-7, move onto application

--- _scripts/lesson1-7.R
+++ _scripts/lesson1-7.R
@@ -15,3 +15,57 @@
   }
   
 }
+
+# 4.0.
+
+{
+  
+  rm(list=ls());
+  
+  yourAge <- as.numeric(readline("What is your age? "));
+  
+  if(yourAge == 20)
+    
+  {
+    
+    cat("Welcome to the third decade of your life!\n");
+    
+  }
+  
+  if(yourAge >= 20)
+    
+  {
+    
+    cat("You still have plenty of life left in you!\n");
+    
+  }
+  
+}
+
+# 5.0.
+
+{
+  
+  rm(list=ls());
+  
+  favAnimal <- readline("What is your favourite animal? ");
+  
+  if(favAnimal == "Llama")
+  
+  {
+    
+    cat("You have the wisdom of the Elders!!")
+    
+  }
+  
+  if(favAnimal != "Llama")
+    
+  {
+    
+    cat("Well, you still have some growing up to do!")
+    
+  }
+  
+}
+
+
